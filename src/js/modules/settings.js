6f2dedf63bcaf73c35141585804528ee2f69701a
Илья Балабуев <balabuev09@gmail.com>
2022-08-07 15:06:17 +0700

    Редактирование настроек

--- src/js/modules/settings.js
+++ src/js/modules/settings.js
@@ -8,6 +8,8 @@ async function init() {
     eventName: "initedAuthorizedUser",
     callback: initCurrentUserInfo,
   });
+
+  initListeners();
 }
 
 function initCurrentUserInfo() {
@@ -31,4 +33,73 @@ function setCardRequisite(card) {
     .split("")
     .slice(-4)
     .join("")}`;
+}
+
+function initListeners() {
+  document
+    .getElementById("email-edit")
+    .addEventListener("submit", async (e) => {
+      e.preventDefault();
+
+      const email = Object.fromEntries(new FormData(e.target)).email;
+
+      const res = await api.updateCurrentUser({
+        login: email,
+      });
+
+      if (res.status === "error") {
+        alert("Ошибка сохранения email");
+        return;
+      }
+
+      setEmail(email);
+      document.location.hash = "";
+    });
+
+  document
+    .getElementById("password-edit")
+    .addEventListener("submit", async (e) => {
+      e.preventDefault();
+
+      const password = Object.fromEntries(new FormData(e.target))["new-pass"];
+
+      const res = await api.updateCurrentUser({
+        password,
+      });
+
+      if (res.status === "error") {
+        alert("Ошибка сохранения password");
+        return;
+      }
+
+      document.location.hash = "";
+    });
+
+  document.getElementById("card-edit").addEventListener("submit", async (e) => {
+    e.preventDefault();
+
+    const formData = Object.fromEntries(new FormData(e.target));
+
+    const cardRequisites = {
+      number: formData["number"],
+      date: formData["date"],
+      cvv: formData["cvv"],
+    };
+
+    const res = await api.updateCurrentUser({
+      cardRequisites: {
+        number: formData["number"],
+        date: formData["date"],
+        cvv: formData["cvv"],
+      },
+    });
+
+    if (res.status === "error") {
+      alert("Ошибка сохранения карты");
+      return;
+    }
+
+    setCardRequisite(cardRequisites);
+    document.location.hash = "";
+  });
 }
