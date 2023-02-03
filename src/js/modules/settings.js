509078e7d376cb5543f725f5070ebf469f729771
Arman Khatoyan <khatarm18@gmail.com>
2023-02-03 14:22:50 +0700

    feat: settings form validation

--- src/js/modules/settings.js
+++ src/js/modules/settings.js
@@ -39,6 +39,83 @@ function getCardNumberWithMask(number) {
   return `${hiddenCardNumber}${number.split("").slice(-4).join("")}`;
 }
 
+function isValidEmail(value) {
+  return value.includes('@');
+}
+
+function isValidNumber(value, countOfNumbers) {
+
+  if (value.length < countOfNumbers) {
+    return false;
+  }
+    
+
+  if (countOfNumbers === 3) {
+    return /^[0-9]{3}$/.test(value);
+  } 
+
+  if (countOfNumbers === 16) {
+    return /^[1-9]{1}[0-9]{15}$/.test(value)
+  } 
+
+  return true;
+}
+
+function isValidPassword(e) {
+  return (e.length >= 6)
+}
+
+function getCoords(elem) {
+  let box = elem.getBoundingClientRect();
+
+  return {
+    top: box.top + window.pageYOffset,
+    right: box.right + window.pageXOffset,
+    bottom: box.bottom + window.pageYOffset,
+    left: box.left + window.pageXOffset
+  };
+}
+
+
+
+function renderToolTip(elem, errorText) {
+
+  const coords = getCoords(elem);
+  const toolContainer = document.createElement('div');
+  const toolText = document.createElement('p')
+
+  toolContainer.classList.add('tool-container');
+  toolText.classList.add('tool-text');
+  toolText.textContent = errorText;
+
+  toolContainer.style.left = coords.right + 'px';
+  toolContainer.style.top = coords.top + 'px';
+  toolContainer.style.zIndex = 3000;
+
+  toolContainer.appendChild(toolText);
+  document.body.appendChild(toolContainer);
+
+  setTimeout(() => {
+    toolContainer.style.visibility = 'hidden'
+  }, 2000)
+}
+
+function cardFormValidator(cardForm) {
+  const cardInputs = cardForm.querySelectorAll('input');
+
+  for (item of cardInputs) {
+    console.log(item.value)
+    const check = isValidNumber(item.value, +(item.getAttribute('maxlength')))
+
+    if (!check || item.value === '') {
+      renderToolTip(item, 'Неверно заполнены данные карты');
+      return false;
+    }
+  }
+
+  return true;
+}
+
 /** Регистрация обработчиков событий. */
 function initListeners() {
     /**
@@ -48,9 +125,65 @@ function initListeners() {
      * - [ ] Реализовать валидацию полей форм по гайдам: https://guides.kontur.ru/principles/validation/
      */
 
+    const emailForm = document.getElementById('email-edit');
+    const passwordForm = document.getElementById('password-edit');
+    const cardForm = document.getElementById('card-edit');
+
+    const emailInput = document.getElementById('settings-email');    
+    const cardNumbInput = document.getElementById('settings-number');  
+    const cardCVCInput = document.getElementById('settings-cvv');  
+    const passInput = document.getElementById('settings-new-pass'); 
+    const repPassInput = document.getElementById('settings-repeat-pass');  
+
+    emailForm.addEventListener('submit', (e) => handleEmailEditSubmit(e));
+    passwordForm.addEventListener('submit', (e) => handlePasswordEditSubmit(e));
+    cardForm.addEventListener('submit', (e) => handleCardEditSubmit(e));
+
+    emailInput.addEventListener('blur', (e) => {
+      if (!isValidEmail(e)) {
+        renderToolTip(e.target, 'Неверное значение email')
+        return;
+      }
+    })
+
+    cardNumbInput.addEventListener('blur', (e) => {
+      if (!isValidNumber(e.target.value, 16)) {
+        renderToolTip(e.target, 'Неверный номер карты')
+        return;
+      }
+    })
+
+    cardCVCInput.addEventListener('blur', (e) => {
+      if (!isValidNumber(e.target.value, 16)) {
+        renderToolTip(e.target, 'Неверный CVC код')
+        return;
+      }
+    })
+
+    passInput.addEventListener('blur', (e) => {
+      if (!isValidPassword(e)) {
+        renderToolTip(e.target, 'Неверный формат пароля')
+        return;
+      }
+    })
+
+    repPassInput.addEventListener('blur', (e) => {
+      if (!isValidPassword(e)) {
+        renderToolTip(e.target, 'Неверный формат пароля')
+        return;
+      }})
+
+
+
     /** Сохранение email. */
     async function handleEmailEditSubmit(e) {
         e.preventDefault();
+
+        if (!isValidEmail(e)) {
+          renderToolTip(e.target, 'Неверное значение email')
+          return;
+        }
+
         const email = Object.fromEntries(new FormData(e.target)).email;
 
         const res = await api.updateCurrentUser({
@@ -69,6 +202,11 @@ function initListeners() {
     /** Сохранение пароля. */
     async function handlePasswordEditSubmit(e) {
         e.preventDefault();
+        
+        if (!isValidPassword(e)) {
+          renderToolTip(e.target, 'Неверный формат пароля')
+          return;
+        }
         const password = Object.fromEntries(new FormData(e.target))["new-pass"];
 
         const res = await api.updateCurrentUser({
@@ -86,6 +224,9 @@ function initListeners() {
     /** Сохранение данных о карте оплаты. */
     async function handleCardEditSubmit(e) {
         e.preventDefault();
+
+        if (!cardFormValidator(e.currentTarget))
+          return;
 
         const formData = Object.fromEntries(new FormData(e.target));
 
